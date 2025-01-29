4e6a70cb7b34f0afe510674cfbd63411f8cf1bf4
Christopher Fortune <cfortune97@gmail.com>
2025-01-29 10:43:35 -0500

    Initial Commit

---
+++ Dex3/Dex3App.swift
@@ -0,0 +1,20 @@
+//
+//  Dex3App.swift
+//  Dex3
+//
+//  Created by Chris Fortune on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Dex3App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
